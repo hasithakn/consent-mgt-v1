0b9949f7bbca1160dc925193fdc8570c0da06f4b
hasithan <hasithakn@hotmail.com>
2025-12-18 23:40:49 +0530

    Update old integration tests

--- consent-server/internal/system/constants/constants.go
+++ consent-server/internal/system/constants/constants.go
@@ -2,28 +2,14 @@ package constants
 
 const (
 	// HTTP Headers
-	AuthorizationHeaderName = "Authorization"
-	ContentTypeHeaderName   = "Content-Type"
+	HeaderContentType       = "Content-Type"
 	CorrelationIDHeaderName = "X-Correlation-ID"
-	OrgIDHeaderName         = "org-id"
-	TPPClientIDHeaderName   = "client-id"
+	HeaderOrgID             = "org-id"
+	HeaderTPPClientID       = "client-id"
 
 	// Content Types
 	ContentTypeJSON = "application/json"
 
-	// Pagination
-	DefaultPageSize = 30
-	MaxPageSize     = 100
-
-	// Token Types
-	TokenTypeBearer = "Bearer"
-
 	// API Base Path
 	APIBasePath = "/api/v1"
-
-	// Aliases for convenience
-	HeaderContentType = ContentTypeHeaderName
-	HeaderOrgID       = OrgIDHeaderName
-	HeaderTPPClientID = TPPClientIDHeaderName
-	ContentTypeJSON2  = ContentTypeJSON
 )
